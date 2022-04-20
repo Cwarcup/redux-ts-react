731784818f35b256e6ab1a2b149dc5106540f092
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-04-20 14:27:22 -0700

    typed selectors

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
+import { useTypedSelector } from '../hooks/useTypeSelector';
 import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
+  console.log(data);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import repositoriesReducer from './repositoriesReducer';
 
+// state is an object with a repositories key
+// and a value from the repositoriesReducer
 const reducers = combineReducers({
   repositories: repositoriesReducer,
 });
 
 export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
+// we now have a type that represents the data in our store.
