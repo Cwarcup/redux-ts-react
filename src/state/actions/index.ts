a4acfcf6acc6439445f3afd0f6a1f3c0d2e966a4
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-04-20 10:08:23 -0700

    reducer

---
+++ src/state/actions/index.ts
@@ -0,0 +1,20 @@
+import { ActionType } from '../action-types';
+
+interface SearchRepoAction {
+  type: ActionType.SEARCH_REPOSITORIES;
+}
+
+interface SearchRepoSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+
+interface SearchRepoErrorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
+
+export type Action =
+  | SearchRepoAction
+  | SearchRepoSuccessAction
+  | SearchRepoErrorAction;
